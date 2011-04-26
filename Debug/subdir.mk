d224edb03f3c5d2cecad199fde6568a981d44a1a
Thorsten Kummerow <thorsten.kummerow@googlemail.com>
2011-04-26 20:15:20 +0200

    Put morse text into program memory. Not tested yet.

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: AVR Compiler'
-	avr-gcc -Wall -g2 -gstabs -O0 -fpack-struct -fshort-enums -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=attiny45 -DF_CPU=1000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o"$@" "$<"
+	avr-gcc -D_DEBUG -Wall -g2 -gstabs -O0 -fpack-struct -fshort-enums -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=attiny45 -DF_CPU=1000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include "main.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/pgmspace.h>
 
 
 #define PAUSE_CHAR 3		// The pause between each letter is 3 "dits"
@@ -16,16 +17,27 @@
 #define LENGTH_DAH 3		// Each "dah" equals 3 "dits" ;-)
 
 
-// The text (ToDo: Put string in program space by using macros defined in avr/pgmspace.h
+// The text (Resides in program space thanks to macros defined in avr/pgmspace.h
 // (see http://www.nongnu.org/avr-libc/user-manual/pgmspace.html))
 
-char *theText = "ein led throwie ist eine led die zusammen mit einer knopfzelle und einem magneten temporaer an objekten befestigt werden kann  die komponenten werden mit epoxidharz oder klebeband fixiert";
 
-// Tests:
-// const char *theText = "e i s h";
-// const char *theText = "eish";
-// const char *theText = "t m o";
-// const char *theText = "tmo";
+char theText[] PROGMEM =
+"ein led-throwie ist eine led, die zusammen mit einer knopfzelle und einem "
+"magneten temporaer an objekten befestigt werden kann. die komponenten werden "
+"mit epoxidharz oder klebeband fixiert. led-throwies wurden vom graffiti research "
+"lab (grl) als eine zerstoerungsfreie alternative zu graffiti erfunden . "
+"led-throwies werden hauptsaechlich fuer streetart oder als effektmittel "
+"fuer events und messen verwendet. in der streetart-szene werden throwies "
+"an metallene objekte wie statuen, bruecken, gebaeude oder oeffentliche "
+"transportmittel geworfen. auf messen und ausstellungen dienen led-throwies "
+"als guenstige beleuchtungsmittel und eyecatcher. "
+"led-throwies wurden 2006 von james powderly und evan roth im graffiti research "
+"lab entwickelt. die throwies wurden wie alle entwicklungen des openlab als open "
+"source-projekt unter public domain veroeffentlicht. "
+"die erste vom graffiti research lab organisierte led-throwie-kampagne fand in new "
+"york city statt. grl-aktivisten verteilten led-throwies an passanten und wiesen sie "
+"an, diese auf die metallskulptur alamo am astor place in manhattan zu werfen. "
+"eine aktion in deutschland wurde am berliner u-bahnhof kottbusser tor durchgefuehrt.";
 
 
 volatile int 	nLenText 	= 0;
@@ -50,15 +62,21 @@ const unsigned char letters[] = {
 };
 
 enum ePunct {
-	Punct_Period,
+	Punct_Period = 0,
 	Punct_Comma,
 	Punct_QuestionMark,
+	Punct_Minus,
+	Punct_LeftBracket,
+	Punct_RightBracket,
 };
 
 const unsigned char punctChars[] = {
 	0b01010110,		// Period
 	0b11001110,		// Comma
 	0b00110010,		// Question mark
+	0b10000110,		// Minus sign
+	0b10110100,		// Left bracket
+	0b10110110,		// Right bracket
 };
 
 unsigned char getMorseChar(char c)
@@ -78,6 +96,18 @@ unsigned char getMorseChar(char c)
 		return punctChars[Punct_QuestionMark];
 		break;
 
+	case '-':
+		return punctChars[Punct_Minus];
+		break;
+
+	case '(':
+		return punctChars[Punct_LeftBracket];
+		break;
+
+	case ')':
+		return punctChars[Punct_RightBracket];
+		break;
+
 	default:
 		return 0;
 	}
@@ -89,28 +119,22 @@ int getCharLength(char c)
 	int nResult = 7;
 	unsigned char bit = 1;
 
-	if ((c < 'a') || (c > 'z')) return 0;
-
 	unsigned char chMorse = getMorseChar(c);
+	if (!chMorse) return 0;
 	while (!(chMorse & bit)) { bit <<= 1; --nResult; }
 
 	return nResult;
 } // getCharLength
 
 
-int strlen(const char* str)
-{
-	int nResult = 0;
-
-	while (*str++) ++nResult;
-	return nResult;
-} // strlen
-
-
 int main()
 {
 	DDRB = _BV(DDB0); 			// Pin 0 = output
+#ifdef _DEBUG
+	PORTB &= ~_BV(PORTB0);		// LED off on bread board
+#else
 	PORTB |= _BV(PORTB0);		// Switch pin 0 "on" to switch the LED off.
+#endif
 
 	// Timer 1 prescaler:
 	TCCR1 = _BV(CS10) | _BV(CS13);	// 1:256
@@ -135,31 +159,42 @@ ISR(TIMER1_OVF_vect)
 	else if (nLenSymb) {
 		--nLenSymb;
 		if (!nLenSymb) {
+#ifdef _DEBUG
+			PORTB &= ~_BV(PORTB0);	// LED off on bread board
+#else
 			PORTB |= _BV(PORTB0);	// LED off
+#endif
+
 			nLenPause = nLenChar ? LENGTH_DIT : LENGTH_DAH;
 		}
 	}
 	else if (nLenChar) {
 		nLenSymb = (uMorse & 0x80) ? LENGTH_DAH : LENGTH_DIT;
+#ifdef _DEBUG
+		PORTB |= _BV(PORTB0);		// LED on on bread board
+#else
 		PORTB &= ~_BV(PORTB0); 		// LED on
+#endif
 		uMorse <<= 1;
 		--nLenChar;
 	}
 	else if (nLenText) {
-		chCur = theText[nPosText++];
+		do {
+			chCur = pgm_read_byte_near(theText + nPosText++);
 
-		if (chCur == ' ') nLenPause = PAUSE_WORD;
-		else {
-			nLenChar = getCharLength(chCur);
-			uMorse = getMorseChar(chCur);
-		}
+			if (chCur == ' ') nLenPause = PAUSE_WORD;
+			else {
+				nLenChar = getCharLength(chCur);
+				uMorse = getMorseChar(chCur);
+			}
 
-		--nLenText;
+			--nLenText;
+		} while (!uMorse && nLenText);		// skip unknown characters
 	}
 	else {
 		// Start over:
 		nLenPause = PAUSE_TEXT;
 		nPosText = 0;
-		nLenText = strlen(theText);
+		nLenText = strlen_P(theText);
 	}
 } // ISR
